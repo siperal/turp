0bb025a733d63de479e9ccd855f74b624ec58594
Frédéric FRANCE <frederic34@users.noreply.github.com>
2025-08-11 19:05:21 +0200

    clean code (#34913)
    * clean code
    
    * Update box_funnel_of_prospection.php
    
    * Update box_funnel_of_prospection.php
    
    ---------
    
    Co-authored-by: Laurent Destailleur <eldy@destailleur.fr>

--- htdocs/core/boxes/box_funnel_of_prospection.php
+++ htdocs/core/boxes/box_funnel_of_prospection.php
@@ -1,10 +1,10 @@
 <?php
 /* Copyright (C) 2012-2014 Charles-François BENKE <charles.fr@benke.fr>
- * Copyright (C) 2014      Marcos García          <marcosgdf@gmail.com>
- * Copyright (C) 2015-2025  Frédéric France        <frederic.france@free.fr>
- * Copyright (C) 2016      Juan José Menent       <jmenent@2byte.es>
- * Copyright (C) 2020      Pierre Ardoin          <mapiolca@me.com>
- * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2014       Marcos García           <marcosgdf@gmail.com>
+ * Copyright (C) 2015-2025  Frédéric France         <frederic.france@free.fr>
+ * Copyright (C) 2016       Juan José Menent        <jmenent@2byte.es>
+ * Copyright (C) 2020       Pierre Ardoin           <mapiolca@me.com>
+ * Copyright (C) 2024		MDW						<mdeweerd@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
