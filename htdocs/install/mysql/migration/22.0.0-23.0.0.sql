7f6faf309df2482c98085d5716fefd77346a5839
ldestailleur <eldy@destailleur.fr>
2025-08-12 05:23:39 +0200

    Clean removed permissions

--- htdocs/install/mysql/migration/22.0.0-23.0.0.sql
+++ htdocs/install/mysql/migration/22.0.0-23.0.0.sql
@@ -78,6 +78,11 @@ ALTER TABLE llx_adherent ADD COLUMN birth_place varchar(64) after birth;
 
 ALTER TABLE llx_societe ADD COLUMN birth date DEFAULT NULL after fk_forme_juridique;
 
+DELETE FROM llx_user_rights WHERE fk_id IN (SELECT id FROM llx_rights_def WHERE module = 'webhook' AND perms = 'webhook_target');
+DELETE FROM llx_usergroup_rights WHERE fk_id IN (SELECT id FROM llx_rights_def WHERE module = 'webhook' AND perms = 'webhook_target');
+
+DELETE FROM llx_rights_def WHERE module = 'webhook' AND perms = 'webhook_target';
+
 ALTER TABLE llx_prelevement_lignes ADD COLUMN bic   varchar(11);   -- 11 according to ISO 9362
 ALTER TABLE llx_prelevement_lignes ADD COLUMN iban	varchar(80);   -- full iban. 34 according to ISO 13616 but we set 80 to allow to store it with encryption information
 ALTER TABLE llx_prelevement_lignes ADD COLUMN rum	varchar(32);   -- rum used
