31f14faa0fe7100b78500ba5203602a0d8d4fc56
Laurent Destailleur <eldy@destailleur.fr>
2025-10-30 17:40:54 +0100

    Debug v23

--- htdocs/core/modules/modSalaries.class.php
+++ htdocs/core/modules/modSalaries.class.php
@@ -105,7 +105,7 @@ class modSalaries extends DolibarrModules
 		$this->rights[$r][5] = '';
 
 		$r++;
-		$this->rights[$r][0] = 511;
+		$this->rights[$r][0] = 512;
 		$this->rights[$r][1] = 'Read employee salaries and payments (yours and your subordinates)';
 		$this->rights[$r][2] = 'r';
 		$this->rights[$r][3] = 0;
