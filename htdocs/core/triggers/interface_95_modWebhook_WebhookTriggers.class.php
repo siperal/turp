1269313304288dacf8221c1678e72411e608acb0
Florian HENRY <florian.henry@open-concept.pro>
2025-12-19 10:15:23 +0100

    fix: Fatal in WebHook Triggers (->errors is null in array merge

--- htdocs/core/triggers/interface_95_modWebhook_WebhookTriggers.class.php
+++ htdocs/core/triggers/interface_95_modWebhook_WebhookTriggers.class.php
@@ -54,6 +54,7 @@ class InterfaceWebhookTriggers extends DolibarrTriggers
 		$this->description = "Webhook triggers.";
 		$this->version = self::VERSIONS['dev'];
 		$this->picto = 'webhook';
+		$this->errors = [];
 	}
 
 	/**
