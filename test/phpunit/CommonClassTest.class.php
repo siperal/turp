234607a3dd1318b24c771b4cb83ec1de6e64cc7d
Laurent Destailleur <eldy@destailleur.fr>
2026-01-29 11:00:43 +0100

    Log

--- test/phpunit/CommonClassTest.class.php
+++ test/phpunit/CommonClassTest.class.php
@@ -221,7 +221,7 @@ abstract class CommonClassTest extends TestCase
 				echo $line . "\n";
 			}
 		} else {
-			echo "Error: File $logFile does not exist or is not readable.\n";
+			echo "File $logFile does not exist or is not readable so we can't show more information.\n";
 		}
 
 
