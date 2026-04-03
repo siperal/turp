f39d98c3c5ce0604a9d501d6f8cd6adde52e139f
Laurent Destailleur <eldy@destailleur.fr>
2026-04-03 04:59:27 +0200

    Change title to 'BrowseBlockedLog' and clear linkback

--- htdocs/blockedlog/admin/blockedlog_control.php
+++ htdocs/blockedlog/admin/blockedlog_control.php
@@ -144,12 +144,12 @@ if ($action == 'control' && $user->hasRight('blockedlog', 'read')) {		// read is
 $form = new Form($db);
 $formother = new FormOther($db);
 
-$title = $langs->trans("ModuleSetup").' '.$langs->trans('BlockedLog');
+$title = $langs->trans("BrowseBlockedLog");
 $help_url = "EN:Module_Unalterable_Archives_-_Logs|FR:Module_Archives_-_Logs_Inaltérable";
 
 llxHeader('', $title, $help_url, '', 0, 0, '', '', '', 'bodyforlist mod-blockedlog page-admin_blockedlog_list');
 
-$linkback = '<a href="'.dolBuildUrl($backtopage ? $backtopage : DOL_URL_ROOT.'/admin/modules.php', ['restore_lastsearch_values' => 1]).'">'.img_picto($langs->trans("BackToModuleList"), 'back', 'class="pictofixedwidth"').'<span class="hideonsmartphone">'.$langs->trans("BackToModuleList").'</span></a>';
+$linkback = '';
 
 $morehtmlcenter = '';
 $texttop = '';
